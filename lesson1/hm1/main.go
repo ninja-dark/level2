2bfb2be6827b22053b26f2fb7a6a574e5e4dfef3
ninja-dark <61248732+ninja-dark@users.noreply.github.com>
2021-07-23 22:55:22 +0300

    added homework

--- lesson1/hm1/main.go
+++ lesson1/hm1/main.go
@@ -1,1 +1,46 @@
-package hm1
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+type MyError struct {
+	time string
+	text string
+}
+
+func New(text string) error{
+	return &MyError{
+		time: timeError(),
+		text: text,
+	}
+}
+
+func(e *MyError) Error() string {
+	return fmt.Sprintf("Time: %s\n Text:\n%s", e.time, e.text)
+}
+
+func timeError() string {
+	time := time.Now()
+	return time.Format( "15:04:05")
+}
+
+func main () {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panic occurred:", err)
+		}
+	}()
+	var a, b, c uint32
+	var err error
+	err = errors.New("my error")
+	fmt.Println(err)
+
+	err = New("my error")
+	fmt.Println(err)
+	a = 12
+	c = a/b
+	fmt.Println("Равно: %d", c)
+
+}
